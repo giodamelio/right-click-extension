0ed4d39c52166a178f78380b9c6b3d3e0a56dfd0
Gio d'Amelio <giovanni.damelio@avalara.com>
2019-02-01 12:49:56 -0800

    Context menu test

--- src/background.js
+++ src/background.js
@@ -1,3 +1,19 @@
 const browser = require('webextension-polyfill');
 
-console.log('Hello World!');
+console.log('Hello World');
+
+browser.menus.create({
+  id: 'test1',
+  title: 'Test 1',
+  contexts: ['selection']
+});
+
+browser.menus.create({
+  id: 'test2',
+  title: 'Test 2',
+  contexts: ['selection']
+});
+
+browser.menus.onClicked.addListener((info, tab) => {
+  console.log(info);
+});
