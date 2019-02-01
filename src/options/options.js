1458784f65d41e104c6fde85b4b3f6f648131ba1
Gio d'Amelio <giovanni.damelio@avalara.com>
2019-02-01 14:04:44 -0800

    Add generated options panel with MSON

--- src/options/options.js
+++ src/options/options.js
@@ -1,4 +1,43 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const Mson = require('mson-react/lib/component').default;
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
+const definition = {
+  component: 'Form',
+  fields: [
+    {
+      name: 'heading',
+      component: 'Text',
+      text: '# Options'
+    },
+    {
+      name: 'jsonConfig',
+      component: 'TextField',
+      required: true,
+      multiline: true
+    },
+    {
+      name: 'save',
+      component: 'ButtonField',
+      label: 'Save',
+      type: 'submit',
+      icon: 'save'
+    }
+  ]
+};
+
+const options = (
+  <Mson
+    definition={definition}
+    onMount={({ component }) => {
+      component.setValues({
+        jsonConfig: '[\n\n]'
+      });
+    }}
+    onSave={({ component }) => {
+      alert(JSON.stringify(component.getValues()));
+    }}
+  />
+);
+
+ReactDOM.render(options, document.getElementById('root'));
