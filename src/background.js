22b916b7ba7700cee3142dc4218c6c565b831947
Gio d'Amelio <giovanni.damelio@avalara.com>
2019-02-06 16:10:07 -0800

    Allow opening of text matching a regex without selection

--- src/background.js
+++ src/background.js
@@ -1,62 +1,119 @@
 const browser = require('webextension-polyfill');
 const util = require('util');
 
-browser.menus.onShown.addListener((info, tab) => {
+// Get the text of an element based on a menu shown event
+function getContextElementText(info, tab) {
+  return browser.tabs.executeScript(tab.id, {
+    frameId: info.frameId,
+    code: `browser.menus.getTargetElement(${info.targetElementId}).innerText;`
+  });
+}
+
+// Add a menu item for each opener
+function addMenuItemsFromOpeners(
+  openers,
+  contexts,
+  matchesRegex = false,
+  textToMatch
+) {
   browser.menus.removeAll();
 
-  return browser.storage.local.get('openers').then(({ openers }) => {
-    // Make a menu item for each opener
-    openers.forEach(opener => {
-      // If the opener has a regex property, only show the menu if the selected text matches it
-      const regex = new RegExp(decodeURIComponent(opener.regex || '.*'));
-      if (regex.test(info.selectionText)) {
-        browser.menus.create({
-          id: opener.name,
-          title: opener.name,
-          contexts: ['selection']
-        });
-      }
-    });
+  console.log(matchesRegex);
 
-    // Create the seperator and options item
+  openers.forEach(opener => {
+    const regex = new RegExp(decodeURIComponent(opener.regex || '.*'));
+    if (matchesRegex && !regex.test(textToMatch)) return;
     browser.menus.create({
-      id: 'separator-1',
-      type: 'separator',
-      contexts: ['selection']
+      id: opener.name,
+      title: opener.name,
+      contexts
     });
-    browser.menus.create({
-      id: 'special:open_options',
-      title: 'Options',
-      contexts: ['selection']
-    });
+  });
 
-    browser.menus.refresh();
+  // Create the seperator and options item
+  browser.menus.create({
+    id: 'separator-1',
+    type: 'separator',
+    contexts
+  });
+  browser.menus.create({
+    id: 'special:open_options',
+    title: 'Options',
+    contexts
+  });
+
+  browser.menus.refresh();
+}
+
+let matchesHash;
+async function onShown(info, tab) {
+  browser.menus.removeAll();
+
+  const { openers } = await browser.storage.local.get('openers');
+
+  if (info.contexts.includes('page')) {
+    // Get the text of the element that was right clicked on
+    const text = await getContextElementText(info, tab);
+
+    // Find openers who's regex matches the text
+    matchesHash = {};
+    const matchingOpeners = openers
+      .filter(opener => opener.regex)
+      .map(opener => {
+        const regex = new RegExp(decodeURIComponent(opener.regex));
+        const regexMatch = text[0].match(regex);
+        const matchedText = regexMatch ? regexMatch[0] : undefined;
+
+        matchesHash[opener.name] = matchedText;
+
+        return {
+          ...opener,
+          matchedText
+        };
+      })
+      .filter(opener => opener.matchedText);
+
+    addMenuItemsFromOpeners(matchingOpeners, ['page']);
+  } else if (info.contexts.includes('selection')) {
+    addMenuItemsFromOpeners(openers, ['selection'], true, info.selectionText);
+  }
+}
+
+async function onClicked(info, tab) {
+  const { openers } = await browser.storage.local.get('openers');
+
+  // Build lookup hash by name
+  const lookup = openers.reduce((l, opener) => {
+    l[opener.name] = opener;
+    return l;
+  }, {});
+
+  const opener = lookup[info.menuItemId];
+  if (opener) {
+    const url = util.format(
+      opener.url,
+      info.selectionText || matchesHash[info.menuItemId]
+    );
+    browser.tabs.create({
+      active: true,
+      url
+    });
+  }
+
+  // Open the options panel
+  if (info.menuItemId === 'special:open_options') {
+    browser.runtime.openOptionsPage();
+  }
+}
+
+browser.menus.onShown.addListener((info, tab) => {
+  return onShown(info, tab).catch(err => {
+    console.error(err);
   });
 });
 
 browser.menus.onClicked.addListener((info, tab) => {
-  console.log(info);
-
-  return browser.storage.local.get('openers').then(({ openers }) => {
-    // Build lookup hash by name
-    const lookup = openers.reduce((l, opener) => {
-      l[opener.name] = opener;
-      return l;
-    }, {});
-
-    const opener = lookup[info.menuItemId];
-    if (opener) {
-      console.log('Opening with:', opener.name);
-      const url = util.format(opener.url, info.selectionText);
-      browser.tabs.create({
-        active: true,
-        url
-      });
-    }
-
-    // Open the options panel
-    if (info.menuItemId === 'special:open_options') {
-      browser.runtime.openOptionsPage();
-    }
+  return onClicked(info, tab).catch(err => {
+    console.error(err);
   });
 });
